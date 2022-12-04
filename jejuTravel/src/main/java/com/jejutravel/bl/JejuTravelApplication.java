90d362ca475a93ffd9979524d4f915e1e6e9e6ed
Hururuha <Hururuha@59.15.223.19>
2022-12-04 19:27:57 +0900

    2022-12-04 rayjo0620 master . . . Create Client branch

---
+++ jejuTravel/src/main/java/com/jejutravel/bl/JejuTravelApplication.java
@@ -0,0 +1,13 @@
+package com.jejutravel.bl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejuTravelApplication.class, args);
+	}
+
+}
